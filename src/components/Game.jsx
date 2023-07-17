7bbd5b7160e13b7738f93390e363c24823dd05b8
Atharv Vani <atharvavani110@gmail.com>
2023-07-17 10:09:30 +0530

    game logic and other pages added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import Swal from "sweetalert2";
+import { Toaster, toast } from "react-hot-toast";
 
-const Game = () => {
+// import data
+import gameData from "../data/gameData";
+
+// import Images
+import pauseBtn from "../assets/pause-btn.svg";
+import submitBtn from "../assets/submit-btn.svg";
+
+// import array data
+
+export default function Game() {
+  const navigate = useNavigate();
+  const [isPlaying, setPlaying] = useState(true);
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+  const [correctCount, setCorrectCount] = useState(0);
+
+  const handleAnswerChange = (event) => {
+    setUserAnswer(event.target.value);
+  };
+
+  const checkAnswer = () => {
+    const correctAnswer = gameData[currentQuestion].answer;
+    const answerIsCorrect =
+      userAnswer.toLowerCase() === correctAnswer.toLowerCase();
+    setIsAnswerCorrect(answerIsCorrect);
+    if (answerIsCorrect) {
+      // console.log("correct");
+      setCorrectCount(correctCount + 1);
+      console.log();
+      setPlaying(false);
+      Swal.fire({
+        allowOutsideClick: false,
+        icon: "success",
+        title: "Right Answer",
+        timer: 1500,
+        showConfirmButton: false,
+      }).then(() => {
+        setPlaying(true);
+      });
+      setUserAnswer("");
+      setIsAnswerCorrect(null);
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      // console.log("wrong");
+      setPlaying(false);
+      Swal.fire({
+        allowOutsideClick: false,
+        icon: "error",
+        title: "Wrong",
+        timer: 1500,
+        showConfirmButton: false,
+      }).then(() => {
+        setPlaying(true);
+      });
+      setUserAnswer("");
+      setIsAnswerCorrect(null);
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  if (currentQuestion >= gameData.length) {
+    navigate("/summary");
+    return <div>Quiz completed!</div>;
+  }
+
+  const currentQuestionObj = gameData[currentQuestion];
+
+  // Pause Function
+  const handlePause = () => {
+    console.log("Pause");
+    setPlaying(false);
+    Swal.fire({
+      allowOutsideClick: false,
+      title: "Game Paused",
+      iconHtml: '<i class="fa-solid fa-pause" style="color: #000000;"></i>',
+      showCancelButton: true,
+      cancelButtonText: "Quit",
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Resume",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setPlaying(true);
+      } else {
+        navigate("/");
+      }
+    });
+  };
+
+  // Complete Funtion
+  const complete = () => {
+    Swal.fire({
+      title: "Time's Up!",
+      iconHtml:
+        '<i class="fa-solid fa-hourglass-end fa-shake" style="color: #000000;"></i>',
+      showCancelButton: false,
+      showConfirmButton: false,
+      text: "Redirecting to summary page...",
+      timer: 2000,
+    }).then(() => {
+      navigate("/summary");
+    });
+  };
+
   return (
-    <div>Game</div>
-  )
+    <div className="game">
+      <Toaster position="bottom-right" reverseOrder={false} />
+      <div className="game-header">
+        <button onClick={handlePause} className="game-pause-btn">
+          <img src={pauseBtn} alt="pause-button-img" />
+        </button>
+        <div className="game-timer">
+          <CountdownCircleTimer
+            strokeLinecap={"round"}
+            size={60}
+            strokeWidth={5}
+            isPlaying={isPlaying}
+            duration={1000}
+            trailColor="#fff"
+            colors="#6C1FCE"
+            onComplete={() => complete()}
+          >
+            {({ remainingTime }) => (
+              <h1 className="game-timer-count">{remainingTime}</h1>
+            )}
+          </CountdownCircleTimer>
+        </div>
+      </div>
+      <div className="game-main">
+        <div className="game-img-card">
+          <img src={currentQuestionObj.image} alt="" />
+          {/* <h1>{currentQuestionObj.question}</h1> */}
+        </div>
+        <h1 className="game-main-heading">{currentQuestionObj.question}</h1>
+        <input
+          className="game-input"
+          type="text"
+          value={userAnswer}
+          onChange={handleAnswerChange}
+          placeholder="Enter your answer"
+        />
+        <button className="game-submit-btn" onClick={checkAnswer}>
+          <img src={submitBtn} alt="submit-button-img" />
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default Game
---
+++ src/data/gameData.js
@@ -0,0 +1,24 @@
+import one from "./apple.png";
+import two from "./pineapple.png"
+import three from  "./orange.png"
+
+const gameData = [
+  {
+    image: one,
+    question:
+      "I am a fruit, round in shape and red in color. I can keep you away from doctors if you eat me every day. Guess who am I?",
+    answer: "apple",
+  },
+  {
+    image: two,
+    question: "What kind of fruit do trees like the most?",
+    answer: "twelve",
+  },
+  {
+    image: three,
+    question: "This is a fruit that You might be fed You just need to mix Yellow and red",
+    answer: "orange",
+  }
+];
+
+export default gameData;
